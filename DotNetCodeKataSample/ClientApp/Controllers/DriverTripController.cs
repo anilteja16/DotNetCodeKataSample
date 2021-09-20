0ff9e2cabfec8c70fa555f132cef4c806f790d59
anilteja16 <90981371+anilteja16@users.noreply.github.com>
2021-09-20 20:24:15 +0530

    Add files via upload

---
+++ DotNetCodeKataSample/ClientApp/Controllers/DriverTripController.cs
@@ -0,0 +1,34 @@
+﻿using DriverTripLibrary;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace DotNetCodeKataSample.Controllers
+{
+   [ApiController]
+   [Route("[controller]")]
+   public class DriverTripController : ControllerBase
+   {
+      private readonly ILogger<DriverTripController> _logger;
+
+      public DriverTripController(ILogger<DriverTripController> logger)
+      {
+         _logger = logger;
+      }
+
+      [HttpPost]
+      public IEnumerable<DriverTripDetails> Post([FromBody] object inputStreamData)
+      {
+         var report = DriverTripReport.GetInstance;
+
+         var streamDataArray = inputStreamData.ToString().Split("\"");
+
+         return report.ProcessInputStream((streamDataArray.Length >= 4) ? streamDataArray[3] : null);
+      }
+
+   }
+}
